bdd124e35e92d71c464fb7b17e948d57312eceb1
Nikolay Dimitrov <nikolaygatev@gmail.com>
2024-04-14 11:50:48 +0300

    Created DatabaseSeeder at TradeSystem.Tests

--- TradeSystem.Tests/FinancialInstrumentServiceTests.cs
+++ TradeSystem.Tests/FinancialInstrumentServiceTests.cs
@@ -1,40 +1,63 @@
-﻿using Moq;
+﻿using Microsoft.EntityFrameworkCore;
+using Moq;
 using TradeSystem.Core.Contracts;
 using TradeSystem.Core.Services;
+using TradeSystem.Data;
+using TradeSystem.Data.Common;
+using TradeSystem.Data.Models;
+using TradeSystem.Data.Repositories;
 
 namespace TradeSystem.Tests
 {
     [TestFixture]
 
-    public class FinancialInstrumentServiceTests : UnitTestsBase
+    public class FinancialInstrumentServiceTests
     {
 
+        private DbContextOptions<TradeSystemDbContext> dbOptions;
+        private TradeSystemDbContext dbContext;
+        private IClientService clientService;
+
+        protected IDeletableEntityRepository<Client> clientRepo;
+        protected IDeletableEntityRepository<Order> orderRepo;
+        protected IDeletableEntityRepository<ClientFinancialInstrument> clientFinInstrRepo;
+        protected IDeletableEntityRepository<FinancialInstrument> financialInstrumentRepo; 
+        protected Mock<IClientService> mockClientService;
+
         [OneTimeSetUp]
-       
-        
+        public void OneTimeSetUp()
+        {
+            this.dbOptions = new DbContextOptionsBuilder<TradeSystemDbContext>()
+                .UseInMemoryDatabase("HouseRentingInMemory" + Guid.NewGuid().ToString())
+                .Options;
+            this.dbContext = new TradeSystemDbContext(this.dbOptions, false);
+
+            this.dbContext.Database.EnsureCreated();
+            TradeSystem.Tests.SeedDatabase.DatabaseSeeder.SeedDatabase(this.dbContext);
+        }
+
         [Test]
 
         public async Task CheckAllFinancialInstrumentsOfClientAsync()
         {
+            clientFinInstrRepo = new EfDeletableEntityRepository<ClientFinancialInstrument>(dbContext);
+            clientRepo = new EfDeletableEntityRepository<Client>(dbContext);
+            orderRepo = new EfDeletableEntityRepository<Order>(dbContext);
+            financialInstrumentRepo = new EfDeletableEntityRepository<FinancialInstrument>(dbContext);
 
-            mockRepoClient.Setup(x => x.AllAsNoTracking()).Returns(listClient.AsQueryable);
-            mockRepoOrder.Setup(x => x.AllAsNoTracking()).Returns(listOrder.AsQueryable);
-            mockRepoClientFinancialInstrument.Setup(x => x.AllAsNoTracking()).Returns(listClientFinancialInstrument.AsQueryable);
-            mockRepoFinancialInstrument.Setup(x => x.AllAsNoTracking()).Returns(listFinancialInstrument.AsQueryable);
-
+            mockClientService = new Mock<IClientService>();
             mockClientService.Setup(x => x.GetClientIdByUserIdAsync(new Guid().ToString()))
                 .ReturnsAsync(new Guid());
-            
-            //var financialInstrumentService = new FinancialInstrumentService(
-            //                                                                    mockRepoClient.Object
-            //                                                                    , mockRepoOrder.Object
-            //                                                                    , mockRepoClientFinancialInstrument.Object
-            //                                                                    , mockRepoFinancialInstrument.Object
-            //                                                                    , mockClientService.Object);
 
-            //var result = await financialInstrumentService.AllFinancialInstrumentsOfClientAsync(null);
+            var financialInstrumentService = new FinancialInstrumentService(
+                                                                                orderRepo
+                                                                                , clientFinInstrRepo
+                                                                                , financialInstrumentRepo
+                                                                                , mockClientService.Object);
 
-            //Assert.IsTrue(result.Count() == 4);
+            var result = await financialInstrumentService.AllFinancialInstrumentsOfClientAsync(null);
+
+            Assert.IsTrue(result.Count() == 4);
 
 
         }
